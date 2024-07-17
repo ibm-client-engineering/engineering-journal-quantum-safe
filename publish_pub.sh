904a5a162876c5f40304bd8c084937dcf153e855
Ross Kramer <kramerro@us.ibm.com>
2024-07-17 13:55:49 -0400

    Adding in some github actions to test building

---
+++ publish_pub.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+GITHUB_SERVER_URL=$1
+GITHUB_REPOSITORY=$2
+
+#PAGES_URL=$(echo "$PAGES_URL" | sed -s "s/https:\/\///")
+
+# GITHUB_REPOSITORY_OWNER = ibm-client-engineering
+# BASE_DOMAIN = github.io
+GITHUB_SERVER_URL="https://github.com"
+GITHUB_SERVER_URL=$(echo "$GITHUB_SERVER_URL" | sed -s "s/https:\/\///")
+GITHUB_REPOSITORY="ibm-client-engineering/engineering-journal-quantum-safe"
+IFS=/ read -r -d '' ORG REPO < <(printf %s "$GITHUB_REPOSITORY")
+
+document="astro.config.mjs"
+
+sed -i "s/\bsite:.*/site: \'https:\/\/$ORG.github.io\',/" $document
+sed -i "s/\bbase:.*/base: \'$REPO\',/" $document
+sed -i "s/\bgithub:.*/github: \'https:\/\/$GITHUB_SERVER_URL\/$ORG\/$REPO\',/" $document
